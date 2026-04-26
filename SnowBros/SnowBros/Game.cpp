44a8ba7eb09f304ffe807f20f79dae7ebc60883a
i253114 <i253114@isb.nu.edu.pk>
2026-04-26 21:47:14 +0500

    level 6

--- SnowBros/SnowBros/Game.cpp
+++ SnowBros/SnowBros/Game.cpp
@@ -142,7 +142,7 @@ void Game::handleMainMenuEvents(sf::Event& event) {
        // player2.loadTexture("assets\\images\\Player_Blue.png");
        //
        // levelsManager.SpecificLevel(playerData.getCurrentLevel());
-        levelsManager.SpecificLevel(4);
+        levelsManager.SpecificLevel(6);
         gameLevel.loadLevel(levelsManager.getCurrentLevel());
         currentState = GameState::PLAYING;
         soundManager.playMusic();
